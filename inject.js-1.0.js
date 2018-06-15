7c19c9a5e5f7c99acebaab8afde2d368e0ed4a52
unknown <jy16353@my.bristol.ac.uk>
2018-06-15 21:31:22 +0800

    Inject js update

--- inject.js-1.0.js
+++ inject.js-1.0.js
@@ -1,19 +1,29 @@
-// Argument toExec is the function to execute, toDefine is a mixed list of
-// statements.
-function injectJS(toExec, toDefine) {
-  let scriptElem = document.createElement('script');
-  let mainFn     = '(' + toExec + ')();';
-  let defined    = '';
+// Codes to indicate function declaration or execution. STR is ignored.
+const FN_DEF  = 0;
+const FN_EXEC = 1;
+const STR     = -1;
 
-  for (int i = 0; i < toDefine.length; i += 1) {
-    let current  = toDefine[i];
-    if (typeof current === 'function' || typeof current === 'string')
-      defined += current + '\n';
-    else return;
+// Argument stmts contain a list of (<Statement>, <Code>), which will be
+// declared/executed in that order.
+function injectJS(stmts) {
+  let scriptElem = document.createElement('script');
+  let scriptText = '';
+
+  for (int i = 0; i < stmts.length; i += 1) {
+    let stmt  = stmts[i];
+
+    if (typeof stmt[0] === 'string') scriptText += stmt[0];
+    else if (typeof stmt[0] === 'function' && stmt[1] === FN_DEF)
+      scriptText += stmt[0];
+    else if (typeof stmt[0] === 'function' && stmt[1] === FN_EXEC)
+      scriptText += '(' + stmt[0] + ')' + '();';
+    else throw 'Invalid script: ' + stmt[0];
+
+    scriptText += '\n';
   }
 
   scriptElem.type        = 'application/javascript';
-  scriptElem.textContent = defined + mainFn;
+  scriptElem.textContent = scriptText;
 
   document.head.appendChild(scriptElem);
   document.head.removeChild(scriptElem);
